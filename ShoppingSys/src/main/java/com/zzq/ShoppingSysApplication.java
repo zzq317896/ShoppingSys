0a9de18fea871c6fbc8cadf7f345e9588d6e59bf
zzq <lenovo@LAPTOP-AHSOCJ2F.mshome.net>
2019-10-31 10:57:16 +0800

    2019-10-31 组长 张自权 项目模板搭建

--- ShoppingSys/src/main/java/com/zzq/ShoppingSysApplication.java
+++ ShoppingSys/src/main/java/com/zzq/ShoppingSysApplication.java
@@ -2,12 +2,17 @@ package com.zzq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 @SpringBootApplication
 public class ShoppingSysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingSysApplication.class, args);
 	}
-
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
 }
